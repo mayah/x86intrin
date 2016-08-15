2e3260da745a0544449a90f4538473dbe0d51621
mayah <mayah@mayah.jp>
2016-08-15 09:21:13 +0900

    Add sse42::mm_crc_*

--- src/sse42.rs
+++ src/sse42.rs
@@ -1,3 +1,14 @@
+extern {
+    #[link_name = "llvm.x86.sse42.crc32.32.8"]
+    fn sse42_crc32_32_8(a: u32, b: u8) -> u32;
+    #[link_name = "llvm.x86.sse42.crc32.32.16"]
+    fn sse42_crc32_32_16(a: u32, b: u16) -> u32;
+    #[link_name = "llvm.x86.sse42.crc32.32.32"]
+    fn sse42_crc32_32_32(a: u32, b: u32) -> u32;
+    #[link_name = "llvm.x86.sse42.crc32.64.64"]
+    fn sse42_crc32_64_64(a: u64, b: u64) -> u64;
+}
+
 // int _mm_cmpestra (__m128i a, int la, __m128i b, int lb, const int imm8)
 // pcmpestri
 // int _mm_cmpestrc (__m128i a, int la, __m128i b, int lb, const int imm8)
@@ -27,11 +38,44 @@
 // int _mm_cmpistrs (__m128i a, __m128i b, const int imm8)
 // pcmpistri
 // int _mm_cmpistrz (__m128i a, __m128i b, const int imm8)
+
 // crc32
 // unsigned int _mm_crc32_u16 (unsigned int crc, unsigned short v)
+#[inline]
+pub fn mm_crc32_u16(crc: u32, v: u16) -> u32 {
+    unsafe { sse42_crc32_32_16(crc, v) }
+}
+
 // crc32
 // unsigned int _mm_crc32_u32 (unsigned int crc, unsigned int v)
+#[inline]
+pub fn mm_crc32_u32(crc: u32, v: u32) -> u32 {
+    unsafe { sse42_crc32_32_32(crc, v) }
+}
+
 // crc32
 // unsigned __int64 _mm_crc32_u64 (unsigned __int64 crc, unsigned __int64 v)
+#[inline]
+pub fn mm_crc32_u64(crc: u64, v: u64) -> u64 {
+    unsafe { sse42_crc32_64_64(crc, v) }
+}
+
 // crc32
 // unsigned int _mm_crc32_u8 (unsigned int crc, unsigned char v)
+#[inline]
+pub fn mm_crc32_u8(crc: u32, v: u8) -> u32 {
+    unsafe { sse42_crc32_32_8(crc, v) }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::*;
+
+    #[test]
+    fn test_crc() {
+        assert_eq!(mm_crc32_u8(1, 100), 1412925310);
+        assert_eq!(mm_crc32_u16(1, 1000), 3870914500);
+        assert_eq!(mm_crc32_u32(1, 50000), 971731851);
+        assert_eq!(mm_crc32_u64(0x000011115555AAAA, 0x88889999EEEE3333), 0x0000000016F57621);
+    }
+}
